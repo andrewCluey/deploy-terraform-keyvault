e9209e495ec658b056ad5ebf8ad1f6beb765dcdf
Andrew Clure <andrew@asc-solutions.co.uk>
2022-05-21 15:21:34 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+locals {
+  default_tags = {
+    environment = var.environment
+  }
+  assigned_tags = merge(local.default_tags, var.tags)
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+
+resource "azurerm_key_vault" "kv" {
+  name                        = var.kv_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = var.soft_delete_retention_days 
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  tags     = local.assigned_tags
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type    = string
+  default = "uksouth"
+}
+variable "environment" {
+  type        = string
+  description = "The staging environment being deployed into"
+  default     = "Dev"
+}
+
+variable "rg_name" {
+  type    = string
+}
+
+variable "soft_delete_retention_days " {
+  type        = number
+  description = "The number of days that soft deleted items should be retained"
+  default     = 7
+}
+
+variable "kv_name" {
+  type        = string
+  description = "The name to assign to the Key Vault."
+}
